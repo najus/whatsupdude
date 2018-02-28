94765b56326a515e3c2d181b71e90ff622bb3aeb
Sujan <najussujan@gmail.com>
2018-02-27 22:43:12 -0500

    Created reciever

---
+++ src/main/java/com/najus/whatsupdude/QuoteResource.java
@@ -0,0 +1,22 @@
+package com.najus.whatsupdude;
+
+public class QuoteResource {
+    String type;
+    Quote value;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Quote getValue() {
+        return value;
+    }
+
+    public void setValue(Quote value) {
+        this.value = value;
+    }
+}
